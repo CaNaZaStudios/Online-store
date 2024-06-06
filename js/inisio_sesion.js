b6170ab4061042ded62a61d5c20886135fcb19a7
CaNaZa <152449595+CaNaZaStudios@users.noreply.github.com>
2024-06-06 18:38:36 -0300

    Add files via upload

---
+++ js/inisio_sesion.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+
+        const storedUsername = localStorage.getItem('storedUsername');
+        const storedPassword = localStorage.getItem('storedPassword');
+
+        if (storedUsername && storedPassword) {
+
+            if (username === storedUsername && password === storedPassword) {
+                alert('Inicio de sesión exitoso');
+                localStorage.removeItem('storedUsername');
+                localStorage.removeItem('storedPassword');
+                window.location.href = '../index.html';
+            } else {
+                alert('Nombre de usuario o contraseña incorrectos');
+            }
+        } else {
+
+            localStorage.setItem('storedUsername', username);
+            localStorage.setItem('storedPassword', password);
+            alert('Por favor, ingrese nuevamente su nombre de usuario y contraseña para confirmar.');
+            loginForm.reset();
+        }
+    });
+});
